63fbd9e18594d9bfd77beb57aa3ff8f0c46df35c
jinseong <jinseong205@gmail.com>
2022-06-17 20:45:31 +0900

    ^^7

---
+++ algorithm/src/algorithm/codingtest/ex02_implements/Ex_2_2.java
@@ -0,0 +1,40 @@
+package algorithm.codingtest.ex02_implements;
+
+public class Ex_2_2 {
+	
+	public static void main(String[] args) {
+		int n = 5;
+		Ex_2_2 sol = new Ex_2_2();
+		System.out.println(sol.solution(n));
+	}
+	
+    public int solution(int n) {
+    	int answer = 0;
+    	
+    	for (int i = 0; i < n + 1; i++) {
+    		if(i % 10 == 3) {
+    			answer += 3600;
+    			System.out.println(i + " answer : "+ answer);
+    			continue;
+    		}
+    		for (int j = 0; j < 60; j ++) {
+    			if(j % 10 == 3 || j/10 == 3) {
+    				answer += 60;
+    				System.out.println(i + ":" + j + " answer : "+ answer);
+    				
+    				continue;
+        		}
+    			for(int k = 0; k < 60; k++) {
+    				if(k % 10 == 3 || k/10 == 3) {
+    					answer += 1;
+    					System.out.println(i + ":" + j + ":" + k + " answer : "+ answer);
+            			
+    				}
+    			}
+    			
+    		}
+    	}
+    	
+    	return answer;
+    }
+}
