86931b199f0f0559683c2d7cf9dbf3379910084c
jinseong <jinseong205@gmail.com>
2022-06-24 18:54:13 +0900

    ^^7

--- algorithm/src/algorithm/codingtest/ex02_implements/Ex_2_2.java
+++ algorithm/src/algorithm/codingtest/ex02_implements/Ex_2_2.java
@@ -1,5 +1,6 @@
 package algorithm.codingtest.ex02_implements;
 
+//상하좌우
 public class Ex_2_2 {
 	
 	public static void main(String[] args) {
@@ -14,21 +15,19 @@ public class Ex_2_2 {
     	for (int i = 0; i < n + 1; i++) {
     		if(i % 10 == 3) {
     			answer += 3600;
-    			System.out.println(i + " answer : "+ answer);
+    			//System.out.println(i + " answer : "+ answer);
     			continue;
     		}
     		for (int j = 0; j < 60; j ++) {
     			if(j % 10 == 3 || j/10 == 3) {
     				answer += 60;
-    				System.out.println(i + ":" + j + " answer : "+ answer);
-    				
+    				//System.out.println(i + ":" + j + " answer : "+ answer);
     				continue;
         		}
     			for(int k = 0; k < 60; k++) {
     				if(k % 10 == 3 || k/10 == 3) {
     					answer += 1;
-    					System.out.println(i + ":" + j + ":" + k + " answer : "+ answer);
-            			
+    					//System.out.println(i + ":" + j + ":" + k + " answer : "+ answer);
     				}
     			}
     			
---
+++ algorithm/src/algorithm/codingtest/ex02_implements/Ex_2_3.java
@@ -0,0 +1,41 @@
+package algorithm.codingtest.ex02_implements;
+
+import java.util.ArrayList;
+
+public class Ex_2_3 {
+	
+	public static void main(String[] args) {
+		Ex_2_3 sol = new Ex_2_3();
+		String str = "a1";
+		System.out.println(sol.solution(str));
+	}
+	
+    public int solution(String str) {
+    	int answer = 0;
+    	
+    	
+    	//a1 -> 치환
+    	int column = str.charAt(0) - 'a' + 1;
+    	int row = Character.getNumericValue(str.charAt(1));
+    	
+    	//System.out.println(column + " "+ row);
+    	
+    	int arr[][] = {
+    					{2,1}, {2,-1}, {1,2}, {1,-2},
+    					{-2,1}, {-2,1}, {-1,2}, {-1, -2}
+    				  };
+    	
+    	//System.out.println(arr.length);
+    	
+    	for (int i =0 ; i < arr.length; i++) {
+    		System.out.println(arr[i][0] + " " + arr[i][1]);
+    		if(column + arr[i][0] > 0 && column + arr[i][0] < 9 
+    		   && row + arr[i][1] > 0 && row +  arr[i][1] < 9) {
+    			answer += 1;
+    			System.out.println("answer: " +arr[i][0] + " " + arr[i][1]);
+    		}
+    	}
+    	
+    	return answer;
+    }
+}
